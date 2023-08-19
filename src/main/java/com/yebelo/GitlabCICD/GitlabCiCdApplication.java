323ef961465f181b6bd0e61326cfcd0c0cba895c
Sandeep M <sandeepm.mice@gmail.com>
2023-08-19 12:32:23 +0530

    first commit

---
+++ src/main/java/com/yebelo/GitlabCICD/GitlabCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.yebelo.GitlabCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yebelo/GitlabCICD/GitlabCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yebelo.GitlabCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
